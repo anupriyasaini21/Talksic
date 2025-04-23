dfaaf137a8a01eadcdeea28f56f0ce641875eed6
Anupriya <170796167+anupriyasaini21@users.noreply.github.com>
2025-04-23 23:37:51 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,18 +1,20 @@
-import MeetingTypeList from "@/components/MeetingTypeList";
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const now = new Date();
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 60000); // update every minute
+    return () => clearInterval(interval);
+  }, []);
 
   const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+  const date = new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' }).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-[url('/images/hero-background.png')] bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          {/* <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
-          </h2> */}
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-[#C9DDFF] lg:text-2xl">{date}</p>
